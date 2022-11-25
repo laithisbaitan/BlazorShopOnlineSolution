39075ff7ec32a467c3e22b529cb6772bd540eae4
laithisbaitan <80163581+laithisbaitan@users.noreply.github.com>
2022-11-25 20:52:19 +0200

    Add project files.

---
+++ ShopOnline.Api/Repositories/ProductRepository.cs
@@ -0,0 +1,28 @@
+﻿using ShopOnline.Api.Entities;
+using ShopOnline.Api.Repositories.Contracts;
+
+namespace ShopOnline.Api.Repositories
+{
+    public class ProductRepository : IProductRepository
+    {
+        public Task<IEnumerable<ProductCategory>> GetCategories()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<ProductCategory> GetCategory(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Product> GetItem(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<Product>> GetItems()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
